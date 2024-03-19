82128510cce4960265856dc5dcc507351e407acd
Mohammed Zaher <mohammedzaherr0@gmail.com>
2024-03-19 19:22:44 +0200

    feat: Add listing filtering by tags

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 
-// All Listings
 Route::get('/', [ListingController::class, 'index']);
 
-// Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
