37e4a465b9abc368280b51daf1947b992c52154b
Mohammed Zaher <mohammedzaherr0@gmail.com>
2024-03-19 17:40:16 +0200

    feat: Add starter theme

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
 
+// All Listings
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => Listing::all(),
+    ]);
+});
+
+// Single Listing
+Route::get('/listings/{id}', function ($id) {
+    return view('listing', [
+        'heading' => 'Listing Details',
+        'listing' => Listing::find($id),
+    ]);
 });
