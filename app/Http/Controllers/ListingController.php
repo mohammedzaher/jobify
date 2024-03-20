4f8558e43324f653126d22395c39a5fa310e51c6
Mohammed Zaher <mohammedzaherr0@gmail.com>
2024-03-20 03:41:02 +0200

    feat: Add delete functionality

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -73,4 +73,10 @@ class ListingController extends Controller
         $listing->update($formFields);
         return back()->with('message', 'Listing updated successfully!');
     }
+
+    public function destroy(Listing $listing)
+    {
+        $listing->delete();
+        return redirect('/')->with('message', 'Listing deleted successfully!');
+    }
 }
